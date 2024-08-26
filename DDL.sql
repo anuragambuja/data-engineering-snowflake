2e074bc66daabaa8671649445a06e0076895a8ec
Anurag Ambuja <anurag.ambuja@gmail.com>
2024-08-26 18:40:17 +0530

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -5,4 +5,55 @@ create database snowflake_sample_data from share sfc_samples.sample_data;
 -- Optionally change the role name to restrict access to a subset of users.
 grant imported privileges on database snowflake_sample_data to role public;
 
+-- 
+CREATE OR REPLACE WAREHOUSE second_warehouse
+with
+WAREHOUSE_SIZE = XSMALL
+AUTO_SUSPEND = 300
+AUTO_RESUME = TRUE 
+SCALING_POLICY = 'Economy'
+MIN_CLUSTER_COUNT = 1
+MAX_CLUSTER_COUNT = 3
+INITIALLY_SUSPENDED = TRUE
+COMMENT = 'This is second warehouse'
+;
 
+DROP WAREHOUSE second_warehouse;
+ALTER WAREHOUSE second_warehouse [RESUME | SUSPEND | SET WAREHOUSE_SIZE = 'SMALL'];
+
+
+//Rename data base & creating the table + meta data
+
+ALTER DATABASE FIRST_DB RENAME TO OUR_FIRST_DB; 
+
+CREATE TABLE "OUR_FIRST_DB"."PUBLIC"."LOAN_PAYMENT" (
+  "Loan_ID" STRING,
+  "loan_status" STRING,
+  "Principal" STRING,
+  "terms" STRING,
+  "effective_date" STRING,
+  "due_date" STRING,
+  "paid_off_time" STRING,
+  "past_due_days" STRING,
+  "age" STRING,
+  "education" STRING,
+  "Gender" STRING);
+  
+  
+ //Check that table is empy
+ USE DATABASE OUR_FIRST_DB;
+
+ SELECT * FROM LOAN_PAYMENT;
+
+ 
+ //Loading the data from S3 bucket
+  
+ COPY INTO LOAN_PAYMENT
+    FROM s3://bucketsnowflakes3/Loan_payments_data.csv
+    file_format = (type = csv 
+                   field_delimiter = ',' 
+                   skip_header=1);
+    
+
+//Validate
+ SELECT * FROM LOAN_PAYMENT;
